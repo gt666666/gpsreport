0346d2236ca6ddcd255e0c76772b8cdc46292076
gt <979627462@qq.com>
2019-09-12 15:10:28 +0800

    Gps数据获取

--- src/test/java/com/ynzhongxi/gpsreport/GpsreportApplicationTests.java
+++ src/test/java/com/ynzhongxi/gpsreport/GpsreportApplicationTests.java
@@ -15,12 +15,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GpsreportApplicationTests {
