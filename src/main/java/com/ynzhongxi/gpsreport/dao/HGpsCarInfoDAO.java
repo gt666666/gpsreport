57c8ee64a8009a897d4f1d904bf35ce9759f89ec
gt <979627462@qq.com>
2019-11-05 14:50:10 +0800

    月分析

--- src/main/java/com/ynzhongxi/gpsreport/dao/HGpsCarInfoDAO.java
+++ src/main/java/com/ynzhongxi/gpsreport/dao/HGpsCarInfoDAO.java
@@ -2,6 +2,7 @@ package com.ynzhongxi.gpsreport.dao;
 
 import cn.hutool.core.util.PageUtil;
 import com.ynzhongxi.gpsreport.pojo.HGpsCarInfo;
+import com.ynzhongxi.gpsreport.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -38,29 +39,60 @@ public class HGpsCarInfoDAO extends BaseMongoDbDao<HGpsCarInfo> {
         tcriteria.and("tired").is("✔");  //疲劳数据不为空
         Query tquery = new Query(tcriteria);
         long tcount = this.mongoTemplate.count(tquery, HGpsCarInfo.class);   //疲劳总量
+        //疲劳车辆车牌号
+        List<HGpsCarInfo> thGpsCarInfos = this.mongoTemplate.find(tquery, HGpsCarInfo.class);
+        StringBuffer   tstr=new StringBuffer();
+        for(int i=0;i<5;i++){
+            tstr.append(thGpsCarInfos.get(i).getCarNumber());
+        }
+        map.put("trCarNumebr",tstr);
+
         map.put("tcount", tcount);
         Criteria scriteria = new Criteria();
         scriteria.and("time").regex(".*?" + month + ".*");
         scriteria.and("speed").is("✔");   //超速数据不为空
         Query squery = new Query(scriteria);
         long scount = this.mongoTemplate.count(squery, HGpsCarInfo.class);   //超速总量
+        //超速车辆车牌号
+        List<HGpsCarInfo> shGpsCarInfos = this.mongoTemplate.find(squery, HGpsCarInfo.class);
+        StringBuffer   sstr=new StringBuffer();
+        if(shGpsCarInfos.size()>6){
+            for(int  i=0;i<5;i++){
+               sstr.append(shGpsCarInfos.get(i).getCarNumber());
+            }
+        }
+        if(shGpsCarInfos.size()<6&&shGpsCarInfos.size()>0){
+            for(int i=0; i<shGpsCarInfos.size();i++){
+                    sstr.append(shGpsCarInfos.get(i).getCarNumber());
+            }
+        }
+        map.put("spCarNumber",sstr.length()>35?sstr.append("......"):sstr);
+
         map.put("scount", scount==0?" ":scount);
         Criteria ocriteria = new Criteria();
         ocriteria.and("month").regex(".*?" + month + ".*");
         ocriteria.and("online").is("是");
         Query oquery = new Query(ocriteria);
         long ocount = this.mongoTemplate.count(oquery, HGpsCarInfo.class);   //在线车辆总量
+         //不在线车辆车牌号
+        Criteria oncriteria = new Criteria();
+        oncriteria.and("month").regex(".*?" + month + ".*");
+        oncriteria.and("online").is("否");
+        Query onquery = new Query(oncriteria);
+        List<HGpsCarInfo> hGpsCarInfos=this.mongoTemplate.find(onquery,HGpsCarInfo.class);
+        StringBuffer   str=new StringBuffer();
+        for(int i=0;i<5;i++){
+            str.append(hGpsCarInfos.get(i).getCarNumber());
+        }
+        map.put("noCarNumber",str);
+
         Criteria ncriteria = new Criteria();
         ncriteria.and("month").regex(".*?" + month + ".*");
         Query nquery = new Query(ncriteria);
         long ncount = this.mongoTemplate.count(nquery, HGpsCarInfo.class);   //每月总在线数量
         map.put("icount", ncount - ocount);
-        // 创建一个数值格式化对象
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        // 设置精确到小数点后1位
-        numberFormat.setMaximumFractionDigits(1);
         if (ocount != 0) {
-            String ns = numberFormat.format((float) ocount / (float) ncount * 100) + "%";   //在线率计算
+            String ns = Tools.getNumberFormat().format((float) ocount / (float) ncount * 100) + "%";   //在线率计算
             map.put("ns", ns);
         }
         if (ocount == 0) {
@@ -68,6 +100,10 @@ public class HGpsCarInfoDAO extends BaseMongoDbDao<HGpsCarInfo> {
         }
         map.put("name", "回通");
         map.put("month",month);
+
+        map.put("iicount", Tools.getNumberFormat().format((float)( ncount - ocount)/(float)ncount*100)+"%"); //不在线所占比率
+        map.put("sscount",Tools.getNumberFormat().format((float) scount / (float) ncount * 100) + "%");   //超速10%所长比率
+        map.put("ttcount",Tools.getNumberFormat().format((float) tcount / (float) ncount * 100) + "%");    //疲劳驾驶所长比率
         return map;
     }
 }
--- src/main/java/com/ynzhongxi/gpsreport/dao/JGpsCarInfoDAO.java
+++ src/main/java/com/ynzhongxi/gpsreport/dao/JGpsCarInfoDAO.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.PageUtil;
 import com.ynzhongxi.gpsreport.pojo.HGpsCarInfo;
 import com.ynzhongxi.gpsreport.pojo.JGpsCarInfo;
 import com.ynzhongxi.gpsreport.utils.DateFormatUtil;
+import com.ynzhongxi.gpsreport.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -46,29 +47,61 @@ public class JGpsCarInfoDAO extends BaseMongoDbDao<JGpsCarInfo> {
         tcriteria.and("tired").is("✔");  //疲劳数据不为空
         Query tquery = new Query(tcriteria);
         long tcount = this.mongoTemplate.count(tquery, JGpsCarInfo.class);   //疲劳总量
+        //疲劳车辆车牌号
+        List<JGpsCarInfo> tjGpsCarInfos = this.mongoTemplate.find(tquery, JGpsCarInfo.class);
+        StringBuffer   tstr=new StringBuffer();
+        for(int i=0;i<5;i++){
+            tstr.append(tjGpsCarInfos.get(i).getCarNumber());
+        }
+        map.put("trCarNumebr",tstr);
+
         map.put("tcount", tcount);
         Criteria scriteria = new Criteria();
         scriteria.and("time").regex(".*?" + month + ".*");
         scriteria.and("speed").is("✔");   //超速数据不为空
         Query squery = new Query(scriteria);
         long scount = this.mongoTemplate.count(squery, JGpsCarInfo.class);   //超速总量
+        //超速车辆车牌号
+        List<JGpsCarInfo> sjGpsCarInfos = this.mongoTemplate.find(squery, JGpsCarInfo.class);
+        StringBuffer   sstr=new StringBuffer();
+        if(sjGpsCarInfos.size()>6){
+            for(int  i=0;i<5;i++){
+                sstr.append(sjGpsCarInfos.get(i).getCarNumber());
+            }
+        }
+        if(sjGpsCarInfos.size()<6&&sjGpsCarInfos.size()>0){
+            for(int i=0; i<sjGpsCarInfos.size();i++){
+                sstr.append(sjGpsCarInfos.get(i).getCarNumber());
+            }
+        }
+        map.put("spCarNumber",sstr.length()>35?sstr.append("......"):sstr);
+
         map.put("scount", scount);
         Criteria ocriteria = new Criteria();
         ocriteria.and("month").regex(".*?" + month + ".*");
         ocriteria.and("online").is("是");
         Query oquery = new Query(ocriteria);
         long ocount = this.mongoTemplate.count(oquery, JGpsCarInfo.class);   //在线车辆总量
+        //不在线车辆车牌号
+        Criteria oncriteria = new Criteria();
+        oncriteria.and("month").regex(".*?" + month + ".*");
+        oncriteria.and("online").is("否");
+        Query onquery = new Query(oncriteria);
+        List<JGpsCarInfo> jGpsCarInfos=this.mongoTemplate.find(onquery,JGpsCarInfo.class);
+        StringBuffer   str=new StringBuffer();
+        for(int i=0;i<5;i++){
+            str.append(jGpsCarInfos.get(i).getCarNumber());
+        }
+        map.put("noCarNumber",str);
+
         Criteria ncriteria = new Criteria();
         ncriteria.and("month").regex(".*?" + month + ".*");
         Query nquery = new Query(ncriteria);
         long ncount = this.mongoTemplate.count(nquery, JGpsCarInfo.class);   //每月总在线数量
         map.put("icount", ncount - ocount);
-        // 创建一个数值格式化对象
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        // 设置精确到小数点后1位
-        numberFormat.setMaximumFractionDigits(1);
+
         if (ocount != 0) {
-            String ns = numberFormat.format((float) ocount / (float) ncount * 100) + "%";   //在线率计算
+            String ns = Tools.getNumberFormat().format((float) ocount / (float) ncount * 100) + "%";   //在线率计算
             map.put("ns", ns);
         }
         if (ocount == 0) {
@@ -76,6 +109,10 @@ public class JGpsCarInfoDAO extends BaseMongoDbDao<JGpsCarInfo> {
         }
         map.put("name", "锦通");
         map.put("month",  month );
+
+        map.put("iicount", Tools.getNumberFormat().format((float)( ncount - ocount)/(float)ncount*100)+"%"); //不在线所占比率
+        map.put("sscount",Tools.getNumberFormat().format((float) scount / (float) ncount * 100) + "%");   //超速10%所长比率
+        map.put("ttcount",Tools.getNumberFormat().format((float) tcount / (float) ncount * 100) + "%");    //疲劳驾驶所长比率
         return map;
     }
 }
